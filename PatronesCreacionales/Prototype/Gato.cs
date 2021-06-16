f67c653bd1deeafb1e77e165ddd28300a62055d5
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-06-16 13:34:22 -0300

    update

---
+++ PatronesCreacionales/Prototype/Gato.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Gato : ICloneable
+    {
+        public int Edad { get; set; }
+        public Rasgos Rasgos { get; set; }
+
+        public object Clone()
+        {
+            //instanciar la misma clase con una clonacion simple
+            //la clonacion simple solo clona propiedades que no sean clases
+            Gato gato = this.MemberwiseClone() as Gato;
+
+            //para clonar tambien propiedades tipo clases hay que instanciar
+            //una nueva clase y asignarla a la propiedad correspondiente
+            Rasgos rasgos = new() 
+            { 
+                Tamaño = this.Rasgos.Tamaño 
+            };
+            gato.Rasgos = rasgos;
+
+            //despues retornar la clase
+            return gato;
+        }
+    }
+
+    public class Rasgos
+    {
+        public string Tamaño { get; set; }
+    }
+}
--- PatronesCreacionales/Prototype/Program.cs
+++ PatronesCreacionales/Prototype/Program.cs
@@ -6,7 +6,20 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Gato gato1 = new() { Edad = 10 };
+            gato1.Rasgos = new Rasgos() { Tamaño = "Grande" };
+            Gato gato2 = gato1.Clone() as Gato;
+            gato2.Edad = 5;
+            gato1.Rasgos.Tamaño = "Mediano";
+
+            Console.WriteLine($"Edad gato1: {gato1.Edad}, " +
+                $"Edad gato2: {gato2.Edad}");
+            Console.WriteLine($"Tamaño gato1: {gato1.Rasgos.Tamaño}, " +
+                $"Tamaño gato2: {gato2.Rasgos.Tamaño}");
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Push any key for exit...");
+            Console.ReadKey();
         }
     }
 }
