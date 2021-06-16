0223da5c5aa5eab3deb2aa474f5a72d88e3ae16f
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-06-15 21:41:29 -0300

    update

---
+++ PatronesCreacionales/Singleton/Calendario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Calendario
+    {
+        private static Calendario instancia = null;
+        private static Dictionary<string, DateTime> eventos = null;
+        public static Calendario Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                    instancia = new Calendario();
+                return instancia;
+            }
+        }
+        public Dictionary<string, DateTime> Eventos { get { return eventos; } }
+        public Calendario()
+        {
+            eventos = new Dictionary<string, DateTime>();
+        }
+
+        public void AddEvento(string evento, DateTime fecha)
+        {
+            eventos.Add(evento, fecha);
+        }
+
+
+        public void PrintFecha() => Console.WriteLine($"La fecha de hoy es {DateTime.Today.ToString("yyyy/MM/dd")}");
+        public void PrintEventos()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Lista de eventos:");
+            foreach (var evento in eventos)
+                Console.WriteLine($"-Evento: {evento.Key} Fecha: {evento.Value.ToString("yyyy/MM/dd")}");
+        }
+
+    }
+}
--- PatronesCreacionales/Singleton/Program.cs
+++ PatronesCreacionales/Singleton/Program.cs
@@ -6,7 +6,19 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calendario.Instancia.PrintFecha();
+
+            Calendario.Instancia.AddEvento("Cumpleaños de Luis", DateTime.Today.AddDays(33));
+            Calendario.Instancia.PrintEventos();
+
+
+            Calendario.Instancia.AddEvento("Salir a caminar", DateTime.Today.AddDays(7));
+            Calendario.Instancia.AddEvento("Clase de ingles", DateTime.Today.AddDays(5));
+            Calendario.Instancia.PrintEventos();
+
+
+            Console.WriteLine("Push any key for exit...");
+            Console.ReadKey();
         }
     }
 }
